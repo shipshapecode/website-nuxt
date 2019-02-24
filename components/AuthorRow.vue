dd7abf22c561846258e328ac6a2798f0fc3e6c2d
Robert Wagner <rwwagner90@gmail.com>
2019-02-23 20:41:03 -0500

    Reconfigure author microdata

--- components/AuthorRow.vue
+++ components/AuthorRow.vue
@@ -1,34 +1,42 @@
 <template>
-  <div
-    class="flex items-center pb-8"
-    itemprop="author"
-    itemscope
-    itemtype="http://schema.org/Person"
-  >
-    <div class="w-12">
-      <img
-        class="h-auto rounded-full w-full"
-        :src="`${image}`"
-        alt="Author image"
-        itemprop="image"
-      >
+  <div class="flex">
+    <div
+      class="flex items-center pb-8"
+      itemprop="author"
+      itemscope
+      itemtype="http://schema.org/Person"
+    >
+      <div class="w-12">
+        <img
+          class="h-auto rounded-full w-full"
+          :src="`${image}`"
+          alt="Author image"
+          itemprop="image"
+        >
+      </div>
+
+      <div class="pl-2 pb-2">
+        By
+
+        <nuxt-link :to="`/blog/authors/${id}/`" itemprop="name">
+          {{ name }}
+        </nuxt-link>
+
+        |
+      </div>
     </div>
 
-    <div class="pl-2 pb-2">
-      By
-
-      <nuxt-link :to="`/blog/authors/${id}/`" itemprop="name">
-        {{ name }}
-      </nuxt-link>
-
-      |
-
-      <time
-        datetime="date"
-        itemprop="datePublished dateModified"
-      >
-        {{ $dateFns.format(date) }}
-      </time>
+    <div
+      class="flex items-center pb-8"
+    >
+      <div class="pl-2 pb-2">
+        <time
+          datetime="date"
+          itemprop="datePublished dateModified"
+        >
+          {{ $dateFns.format(date) }}
+        </time>
+      </div>
     </div>
   </div>
 </template>
