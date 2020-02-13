3e9f0e71faf4c27bbaedcd788e16fa99dc2a46e6
Robert Wagner <rwwagner90@gmail.com>
2020-02-13 08:20:03 -0500

    Update ServicesBlurbs.vue

--- components/ServicesBlurbs.vue
+++ components/ServicesBlurbs.vue
@@ -3,17 +3,17 @@
     <div class="section-content">
       <div class="flex flex-wrap items-center">
         <div
-          class="bg-white flex flex-grow m-2 p-6 justify-center items-center lg:flex-1 lg:h-80 lg:ml-0"
+          class="bg-white flex flex-grow m-2 p-12 justify-center lg:flex-1 lg:h-96 lg:ml-0"
           data-aos="fade-up"
           itemscope
           itemtype="http://schema.org/Service"
         >
-          <div class="m-auto max-w-2xl w-full">
-            <h3 class="heading" itemprop="name">
+          <div class="flex flex-col max-w-2xl w-full">
+            <h3 class="heading mt-4" itemprop="name">
               Ember Consulting
             </h3>
 
-            <p>
+            <p class="flex-grow">
               Ember.js is Ship Shape’s framework of choice, among the multitude
               of other JavaScript frameworks out there. With Ember, we can
               accomplish so much more - faster, with less errors and on a larger
@@ -28,17 +28,17 @@
         </div>
 
         <div
-          class="bg-white flex flex-grow m-2 p-6 justify-center items-center lg:flex-1 lg:h-80 lg:mr-0"
+          class="bg-white flex flex-grow m-2 p-12 justify-center lg:flex-1 lg:h-96 lg:mr-0"
           data-aos="fade-up"
           itemscope
           itemtype="http://schema.org/Service"
         >
-          <div class="m-auto max-w-2xl w-full">
-            <h3 class="heading" itemprop="name">
+          <div class="flex flex-col max-w-2xl w-full">
+            <h3 class="heading mt-4" itemprop="name">
               App Development
             </h3>
 
-            <p>
+            <p class="flex-grow">
               Building custom full stack web apps is our speciality. Ship Shape
               designs, engineers and programs web apps from start-to-finish so
               that user experience is flawless and reliable for years to come.
@@ -54,18 +54,18 @@
 
       <div class="flex flex-wrap items-center">
         <div
-          class="bg-white flex flex-grow m-2 p-6 justify-center items-center lg:flex-1 lg:h-80 lg:ml-0"
+          class="bg-white flex flex-grow m-2 p-12 justify-center lg:flex-1 lg:h-96 lg:ml-0"
           data-aos="fade-up"
           itemscope
           itemtype="http://schema.org/Service"
         >
-          <div class="m-auto max-w-2xl w-full">
-            <h3 class="heading" itemprop="name">
+          <div class="flex flex-col max-w-2xl w-full">
+            <h3 class="heading mt-4" itemprop="name">
               Mentoring and Team Training
             </h3>
 
-            <p>
-              Whether you’re a Senior Developer or Junior Developer, Ship Shape
+            <p class="flex-grow">
+              Whether you're a Senior Developer or Junior Developer, Ship Shape
               fosters one-on-one connections that help make a difference in our
               community. We offer several Ember mentorship programs that
               identify your goals and prepare you and your team for the future
@@ -80,17 +80,17 @@
         </div>
 
         <div
-          class="bg-white flex flex-grow m-2 p-6 justify-center items-center lg:flex-1 lg:h-80 lg:mr-0"
+          class="bg-white flex flex-grow m-2 p-12 justify-center lg:flex-1 lg:h-96 lg:mr-0"
           data-aos="fade-up"
           itemscope
           itemtype="http://schema.org/Service"
         >
-          <div class="m-auto max-w-2xl w-full">
-            <h3 class="heading" itemprop="name">
+          <div class="flex flex-col max-w-2xl w-full">
+            <h3 class="heading mt-4" itemprop="name">
               App Tours
             </h3>
 
-            <p>
+            <p class="flex-grow">
               We built our own open source library,
               <a href="https://shepherdjs.dev/">Shepherd.js</a> to guide users
               through the features and functions of your web app. It’s no
@@ -108,17 +108,17 @@
 
       <div class="flex flex-wrap items-center">
         <div
-          class="bg-white flex flex-grow m-2 p-6 justify-center items-center lg:flex-1 lg:h-80 lg:ml-0"
+          class="bg-white flex flex-grow m-2 p-12 justify-center lg:flex-1 lg:h-96 lg:ml-0"
           data-aos="fade-up"
           itemscope
           itemtype="http://schema.org/Service"
         >
-          <div class="m-auto max-w-2xl w-full">
-            <h3 class="heading" itemprop="name">
+          <div class="flex flex-col max-w-2xl w-full">
+            <h3 class="heading mt-4" itemprop="name">
               Sponsored Open Source
             </h3>
 
-            <p>
+            <p class="flex-grow">
               Ship Shape has created several data adapters, add-ons, and
               libraries to fit into your company’s framework.
             </p>
@@ -131,17 +131,17 @@
         </div>
 
         <div
-          class="bg-white flex flex-grow m-2 p-6 justify-center items-center lg:flex-1 lg:h-80 lg:mr-0"
+          class="bg-white flex flex-grow m-2 p-12 justify-center lg:flex-1 lg:h-96 lg:mr-0"
           data-aos="fade-up"
           itemscope
           itemtype="http://schema.org/Service"
         >
-          <div class="m-auto max-w-2xl w-full">
-            <h3 class="heading" itemprop="name">
+          <div class="flex flex-col max-w-2xl w-full">
+            <h3 class="heading mt-4" itemprop="name">
               Nuxt.js Consulting
             </h3>
 
-            <p>
+            <p class="flex-grow">
               Nuxt.js makes progressive web app development simple, powerful and
               user friendly. Stay ahead of tech giants and build a solid
               foundation for your digital future with Ship Shape’s Nuxt
