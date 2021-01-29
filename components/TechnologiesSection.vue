ecb10bd1ef588c856ee7aafb34c67426c2df4052
Robert Wagner <rwwagner90@gmail.com>
2021-01-29 16:55:11 -0500

    Start updating technologies section

--- components/TechnologiesSection.vue
+++ components/TechnologiesSection.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="section flex flex-wrap justify-center lg:mb-12">
-    <div class="section-content" data-aos="flip-down">
+    <div
+      class="section-content bg-grey-transparent p-8 lg:p-16"
+      data-aos="flip-down"
+    >
       <div class="flex flex-wrap">
-        <div class="w-full lg:w-1/5 text-center lg:text-left">
-          <h3 class="text-4xl lg:text-5xl xl:text-6xl">Technologies we use</h3>
+        <div class="w-full text-center lg:text-left">
+          <h3 class="text-4xl lg:text-5xl">Technologies we use</h3>
         </div>
 
-        <div class="flex flex-wrap w-full lg:pl-32 lg:w-4/5 text-grey">
-          <div class="w-1/2 sm:w-1/3 flex justify-center items-center">
+        <div class="grid grid-cols-2 lg:grid-cols-3 text-grey">
+          <div>
             <a href="https://emberjs.com/" aria-label="Ember.js">
               <inline-svg
                 class="w-full h-full p-8 max-w-xxs"
@@ -15,7 +18,7 @@
               />
             </a>
           </div>
-          <div class="w-1/2 sm:w-1/3 flex justify-center items-center">
+          <div>
             <a href="https://www.electronjs.org/" aria-label="Electron">
               <inline-svg
                 class="w-full h-full p-8 max-w-xxs"
@@ -23,7 +26,7 @@
               />
             </a>
           </div>
-          <div class="w-1/2 sm:w-1/3 flex justify-center items-center">
+          <div>
             <a href="https://tailwindcss.com/" aria-label="Tailwind CSS">
               <inline-svg
                 class="w-full h-full p-8 max-w-xxs"
@@ -31,7 +34,7 @@
               />
             </a>
           </div>
-          <div class="w-1/2 sm:w-1/3 flex justify-center items-center">
+          <div>
             <a href="https://nuxtjs.org/" aria-label="Nuxt.js">
               <inline-svg
                 class="w-full h-full p-8 max-w-xxs"
@@ -39,7 +42,7 @@
               />
             </a>
           </div>
-          <div class="w-1/2 sm:w-1/3 flex justify-center items-center">
+          <div>
             <a href="https://www.serverless.com/" aria-label="Serverless">
               <inline-svg
                 class="w-full h-full p-8 max-w-xxs"
@@ -47,7 +50,7 @@
               />
             </a>
           </div>
-          <div class="w-1/2 sm:w-1/3 flex justify-center items-center">
+          <div>
             <a href="https://nextjs.org/" aria-label="Next.js">
               <inline-svg
                 class="w-full h-full p-8 max-w-xxs"
