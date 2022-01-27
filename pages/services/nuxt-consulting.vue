a1461784a751fc274744485c5ee8694d4bb2fcdf
Hanna (she/her) <62962173+hannakim91@users.noreply.github.com>
2022-01-27 08:07:03 -0700

    Nuxt js page (#217)
    * style: full-size page design for nuxt page
    
    - took a couple elements from robbie's next-js page branch
    
    * style: change blue bg section to mobile-first
    
    * style: update next sections to mobile-first
    
    - rough waters
    - client feedback box
    
    * style: open source section re-do to mobile-first
    
    * style: fix whitespace
    
    * style: cta button at different breakpoints
    
    * style: whitespace adjustments
    
    * style: fix spacing under header
    
    * style: fix image-text ratio
    
    * style: update font sizes and white spaces for mobile view
    
    * style: edit medium size white space
    
    * style: limit image sizes
    
    * Add some sections and update some styles
    
    * Reset some components
    
    * Update waves and spacing
    
    Co-authored-by: Robert Wagner <rwwagner90@gmail.com>

--- pages/services/nuxt-consulting.vue
+++ pages/services/nuxt-consulting.vue
@@ -1,16 +1,16 @@
 <template>
   <div itemscope itemtype="http://schema.org/Service">
-    <HeroBlock hero-classes="nuxt-consulting">
+    <HeroBlock
+      hero-classes="nuxt-consulting waves nuxt-waves !pb-48 2xl:!pb-56"
+    >
       <template #content>
-        <h1 itemprop="name">Nuxt Consulting</h1>
-        <p>
-          Nuxt.js is a higher-level framework that simplifies the development of
-          universal or single page Vue apps. With Nuxt.js, Ship Shape's team can
-          write and build universal apps simply.
-        </p>
+        <h1 itemprop="name" class="leading-tight text-5xl">
+          Create a Nuxt.js app that's universal — without all the hassle
+        </h1>
+
         <div>
-          <nuxt-link class="btn btn-red" to="/contact/">
-            Get started
+          <nuxt-link class="btn btn-red w-full lg:w-auto" to="/contact/">
+            Let's get started
           </nuxt-link>
         </div>
       </template>
@@ -26,88 +26,209 @@
       </template>
     </HeroBlock>
 
-    <div class="bg-blue-light section flex flex-wrap justify-center">
-      <div class="section-content">
-        <h2 class="text-center text-navy w-full">Why use Nuxt?</h2>
+    <div class="section bg-navy flex flex-wrap justify-center">
+      <div class="section-content" data-aos="fade-up">
+        <h2 class="text-white text-3xl">All about Nuxt.js</h2>
+        <div
+          class="
+            grid
+            space-y-12
+            pt-10
+            pb-32
+            lg:pt-4 lg:grid-cols-8 lg:gap-20 lg:space-y-0
+          "
+        >
+          <div
+            class="
+              bg-blue-light
+              flex
+              items-center
+              p-12
+              rounded-md
+              text-navy
+              lg:col-span-3
+            "
+          >
+            <p class="mb-0">
+              Nuxt.js is an open-source framework used to build Vue.js web
+              applications. It’s a performant system that makes creating
+              universal apps easier because of its reduced routing complexixty,
+              giving you SEO benefits right out of the box.
+            </p>
+          </div>
 
-        <div class="grid grid-cols-1 gap-6 lg:grid-cols-2">
-          <div class="bg-navy p-6 lg:p-12">
+          <p class="text-white lg:col-span-5 lg:content-center lg:p-8">
+            At Ship Shape, we love Nuxt.js because it’s still an opinionated
+            framework (though not quite as opinionated as Ember.js), and it has
+            features that strip out unused code to make your app fast and
+            lightweight. You’ll enjoy its approachable syntax, easy-to-read read
+            templates, and more.
+            <br />
+            <br />
+            Whether you're building a brand new web app or elevating an existing
+            experience with the latest best practices, our first-class engineers
+            can refine or create a Nuxt.js app while training your team in the
+            process.
+          </p>
+        </div>
+
+        <h5 class="text-white" data-aos="fade">
+          Nuxt works well for apps big and small thanks to its:
+        </h5>
+
+        <div
+          class="
+            divide-grey-light divide-y
+            grid grid-cols-1
+            my-20
+            text-white
+            lg:divide-x lg:divide-y-0 lg:grid-cols-3
+          "
+        >
+          <div class="flex flex-col items-center px-16" data-aos="flip-down">
             <nuxt-img
               class="h-20 inline-block mb-8 w-auto"
               src="/svgs/icons/modular.svg"
             />
 
-            <h5 class="text-white">Modular</h5>
-
-            <p class="text-white">
-              Nuxt is based on a powerful modular architecture. You can choose
-              from more than 50 modules to make your development faster and
-              easier. You don't have to reinvent the wheel to get PWA benefits,
-              add Google Analytics to your page or generate a sitemap.
+            <p class="mb-12 text-center lg:m-0 lg:w-64">
+              Modular architecture, which speeds up development
             </p>
           </div>
-          <div class="bg-navy p-6 lg:p-12">
+
+          <div
+            class="flex flex-col items-center px-16"
+            data-aos="flip-down"
+            data-aos-delay="200"
+          >
             <nuxt-img
-              class="h-20 inline-block mb-8 w-auto"
-              src="/svgs/icons/chart.svg"
+              class="h-20 inline-block mb-8 mt-12 w-auto lg:mt-0"
+              src="/svgs/icons/native-apps.svg"
             />
 
-            <h5 class="text-white">Performant</h5>
-
-            <p class="text-white">
-              With Nuxt.js, your application will be optimized out of the box.
-              We do our best to build performant applications by utilizing
-              Vue.js and Node.js best practices. To squeeze every unnecessary
-              bit out of your app Nuxt includes a bundle analyzer and lots of
-              opportunities to fine-tune your app.
+            <p class="mb-12 text-center lg:m-0 lg:w-64">
+              Server-side rendered (SSR) to deliver HTML, rather than JavaScript
             </p>
           </div>
 
-          <div class="bg-navy p-6 lg:p-12">
+          <div
+            class="flex flex-col items-center px-16"
+            data-aos="flip-down"
+            data-aos-delay="400"
+          >
             <nuxt-img
-              class="h-20 inline-block mb-8 w-auto"
-              src="/svgs/icons/fun.svg"
-            />
-
-            <h5 class="text-white">Enjoyable</h5>
-
-            <p class="text-white">
-              Nuxt takes developer experience seriously, and is always improving
-              to make using it more enjoyable. Expect appealing solutions,
-              descriptive error messages, powerful defaults and detailed
-              documentation.
-            </p>
-          </div>
-
-          <div class="bg-navy p-6 lg:p-12">
-            <nuxt-img
-              class="h-24 inline-block mb-4 w-24"
+              class="h-20 inline-block mb-8 mt-12 w-auto lg:mt-0"
               src="/svgs/icons/static.svg"
             />
 
-            <h5 class="text-white">Static / PWA</h5>
-
-            <p class="text-white">
-              Nuxt.js supports several rendering modes, including server side
-              rendering or "universal", static or "JAMStack", and classic single
-              page application (SPA) mode to allow for maximum flexibility out
-              of the box.
+            <p class="mb-12 text-center lg:m-0 lg:w-64">
+              Static Site Generation to net SEO benefits without needing a
+              server
             </p>
           </div>
         </div>
       </div>
     </div>
+
+    <div
+      class="section flex flex-wrap justify-center !pt-40"
+      data-aos="fade-down"
+    >
+      <div
+        class="
+          section-content
+          gap-20
+          grid grid-cols-1
+          items-center
+          lg:gap-32 lg:grid-cols-2
+        "
+      >
+        <div>
+          <nuxt-img
+            class="h-auto m-auto max-w-2xl w-full"
+            format="webp"
+            src="/img/hero-images/mentoring.png"
+            height="772"
+            width="1312"
+          />
+        </div>
+        <div>
+          <h3>Your first mate's in rough waters</h3>
+          <p>
+            Ship Shape is committed to leaving every engineering team we work
+            with better than we found it. Not only do we build robust, reliable
+            software, but we also train your staff along the way to understand,
+            maintain, and improve your product after our engagement ends.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <SingleQuote :testimonial="testimonial" />
+
+    <div class="section flex flex-wrap justify-center">
+      <div
+        class="
+          section-content
+          gap-20
+          grid grid-cols-1
+          items-center
+          lg:gap-32 lg:grid-cols-2
+        "
+      >
+        <div class="lg:order-last" data-aos="fade-up">
+          <nuxt-img
+            class="h-auto m-auto max-w-2xl w-full"
+            format="webp"
+            src="/img/hero-images/ember-consulting.png"
+            height="1034"
+            width="1312"
+          />
+        </div>
+        <div>
+          <h3 data-aos="fade-up">We ❤️ the Nuxt.js Community</h3>
+          <h4 class="text-2xl" data-aos="fade-up">Proud Nuxt.js partners</h4>
+          <p data-aos="fade-up">
+            Ship Shape proudly
+            <a href="https://nuxtjs.org/partners/ship-shape"
+              >sponsors Nuxt.js</a
+            >
+            to support its talented team and dynamic framework. Our partnership
+            is one of the main reasons we’re always ready to create Nuxt.js
+            apps: because we stay up-to-date with the Nuxt team and give back to
+            the community.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <WaveMoreLinks />
   </div>
 </template>
 
 <script>
 import { generateMeta } from '~/utils/meta';
+const testimonial = {
+  name: 'Donald Piret',
+  title: 'Director of Engineering at Sephora',
+  imgSrc: 'donald',
+  svgSrc: '/svgs/clients/sephora-white.svg',
+  quote: `Ship Shape delivered beyond our expectations. They pointed out things
+          we had never even thought of or knew about. Our team learned a huge
+          amount of new information and new tricks, and we still have them on
+          retainer for very specific questions. Plus, Ship Shape is the
+          friendliest team of engineers I've worked with.`
+};
 
 export default {
   speedkitComponents: {
-    HeroBlock: () => import('@/components/HeroBlock')
+    HeroBlock: () => import('@/components/HeroBlock'),
+    SingleQuote: () => import('@/components/SingleQuote'),
+    WaveMoreLinks: () => import('@/components/WaveMoreLinks')
   },
-
+  data() {
+    return { testimonial };
+  },
   head() {
     const title = 'Nuxt.js Software Consultants & Developers';
     const description =
