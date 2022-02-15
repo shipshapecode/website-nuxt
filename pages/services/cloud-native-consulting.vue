035ea6214e9d31ca6c06b978adf205b1e83d4201
hannakim91 <hannakim91@gmail.com>
2022-02-14 22:30:30 -0800

    refactor: update cloud native page with AllAboutSpecialties component

--- pages/services/cloud-native-consulting.vue
+++ pages/services/cloud-native-consulting.vue
@@ -25,120 +25,10 @@
       </template>
     </HeroBlock>
 
-    <div class="section bg-navy flex flex-wrap justify-center">
-      <div class="section-content">
-        <div class="grid grid-cols-1 gap-20 mb-32 lg:gap-32 lg:grid-cols-2">
-          <div class="text-white" data-aos="fade-up">
-            <h2 class="mb-10 text-4xl text-white">
-              All about cloud-native applications
-            </h2>
-
-            <p>
-              Cloud-native development is a methodology for building and running
-              scalable applications in modern, dynamic environments. This
-              platform-agnostic approach gives you flexibility so you don’t have
-              to work with one vendor (such as AWS or Azure) and provides
-              engineering teams with tools to set up a clear, effective software
-              delivery pipeline so they can deliver battle-tested, secure
-              solutions faster.
-            </p>
-          </div>
-
-          <div
-            class="
-              bg-blue-light
-              flex
-              items-center
-              p-12
-              rounded-md
-              text-navy
-              lg:mt-16
-            "
-            data-aos="fade-up"
-            data-aos-delay="200"
-          >
-            <p class="mb-0">
-              If you’re working on developing cloud-native applications or would
-              like to explore the value they could bring to your team and
-              customers, look no further. Our cloud-native specialists can help
-              you build efficient and effective products today, lay a solid
-              foundation for the future, and upskill your team so they can
-              continue making positive progress long after our engagement ends.
-            </p>
-          </div>
-        </div>
-
-        <h3 class="text-white text-2xl" data-aos="fade">
-          Ship Shape’s engineering staff prefers using a cloud-native approach
-          because of its:
-        </h3>
-
-        <div
-          class="
-            divide-grey-light divide-y
-            grid grid-cols-1
-            my-20
-            text-white
-            lg:divide-x lg:divide-y-0 lg:grid-cols-4
-          "
-        >
-          <div class="flex flex-col items-center px-16" data-aos="flip-down">
-            <nuxt-img
-              alt=""
-              class="h-20 inline-block mb-8 w-auto"
-              src="/svgs/icons/retainer.svg"
-            />
-
-            <p class="mb-12 text-center lg:m-0 lg:w-40">
-              Fast, transparent software delivery cycle
-            </p>
-          </div>
-
-          <div
-            class="flex flex-col items-center px-16"
-            data-aos="flip-down"
-            data-aos-delay="200"
-          >
-            <nuxt-img
-              alt=""
-              class="h-20 inline-block mb-8 mt-12 w-auto lg:mt-0"
-              src="/svgs/icons/speed.svg"
-            />
-            <p class="mb-12 text-center lg:m-0 lg:w-40">
-              Ability to deliver value to customers even faster
-            </p>
-          </div>
-
-          <div
-            class="flex flex-col items-center px-16"
-            data-aos="flip-down"
-            data-aos-delay="400"
-          >
-            <nuxt-img
-              alt=""
-              class="h-20 inline-block mb-8 mt-12 w-auto lg:mt-0"
-              src="/svgs/icons/secure.svg"
-            />
-            <p class="mb-12 text-center lg:m-0 lg:w-40">
-              Tested, safe, and secure deployment
-            </p>
-          </div>
-
-          <div
-            class="flex flex-col items-center px-16"
-            data-aos="flip-down"
-            data-aos-delay="600"
-          >
-            <nuxt-img
-              alt=""
-              class="h-20 inline-block mb-8 mt-12 w-auto lg:mt-0"
-              src="/svgs/icons/chart.svg"
-            />
-            <p class="mb-12 text-center lg:m-0 lg:w-40">Rapid scalability</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <AllAboutSpecialty
+      :text="specialtyDetails.text"
+      :benefits="specialtyDetails.benefits"
+    />
 
     <div
       class="section flex flex-wrap justify-center !pt-40"
@@ -214,28 +104,58 @@
 <script>
 import { generateMeta } from '~/utils/meta';
 
+const specialtyDetails = {
+  text: {
+    h2: 'All about Cloud Native',
+    h3: 'Ship Shape’s engineering staff prefers using a cloud-native approach because of its:',
+    boxedText:
+      'If you’re working on developing cloud-native applications or would like to explore the value they could bring to your team and customers, look no further. Our cloud-native specialists can help you build efficient and effective products today, lay a solid foundation for the future, and upskill your team so they can continue making positive progress long after our engagement ends.',
+    unboxedText:
+      'Cloud-native development is a methodology for building and running scalable applications in modern, dynamic environments. This platform-agnostic approach gives you flexibility so you don’t have to work with one vendor (such as AWS or Azure) and provides engineering teams with tools to set up a clear, effective software delivery pipeline so they can deliver battle-tested, secure solutions faster.'
+  },
+  benefits: [
+    {
+      imgSrc: '/svgs/icons/retainer.svg',
+      description: 'Fast, transparent software delivery cycle'
+    },
+    {
+      imgSrc: '/svgs/icons/speed.svg',
+      description: 'Ability to deliver value to customers even faster'
+    },
+    {
+      imgSrc: '/svgs/icons/secure.svg',
+      description: 'Tested, safe, and secure deployment'
+    },
+    {
+      imgSrc: '/svgs/icons/chart.svg',
+      description: 'Rapid scalability'
+    }
+  ]
+};
+
 const testimonial = {
   name: 'Harley Sugarman',
   title: 'Founder, CEO at Enigma',
   imgSrc: 'harley',
   svgSrc: '/svgs/clients/enigma.svg',
-  quote: `I loved working with Ship Shape. 
-          They helped our small team build a product from the ground up 
-          and were strong partners every step of the way. Their domain expertise 
-          in Next.js and React helped lay the foundation for a scalable codebase, 
-          and they were great at ensuring we adhered to engineering best practices. 
-          Every member of the team was a pleasure to work with and (best of all) 
+  quote: `I loved working with Ship Shape.
+          They helped our small team build a product from the ground up
+          and were strong partners every step of the way. Their domain expertise
+          in Next.js and React helped lay the foundation for a scalable codebase,
+          and they were great at ensuring we adhered to engineering best practices.
+          Every member of the team was a pleasure to work with and (best of all)
           we ended up launching our MVP on schedule!`
 };
 
 export default {
   speedkitComponents: {
+    AllAboutSpecialty: () => import('@/components/AllAboutSpecialty'),
     HeroBlock: () => import('@/components/HeroBlock'),
     SingleQuote: () => import('@/components/SingleQuote'),
     WaveMoreLinks: () => import('@/components/WaveMoreLinks')
   },
   data() {
-    return { testimonial };
+    return { testimonial, specialtyDetails };
   },
   head() {
     const title = 'Next.js Software Consultants & Developers';
