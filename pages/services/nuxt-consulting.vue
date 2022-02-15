4a7400a989fed565bdab9a7f7824b39e28b800b4
hannakim91 <hannakim91@gmail.com>
2022-02-14 22:19:52 -0800

    refactor: nuxt page with AllAboutSpecialty component

--- pages/services/nuxt-consulting.vue
+++ pages/services/nuxt-consulting.vue
@@ -26,6 +26,12 @@
       </template>
     </HeroBlock>
 
+    <AllAboutSpecialty
+      :text="specialtyDetails.text"
+      :benefits="specialtyDetails.benefits"
+      :flip-paragraphs="true"
+    />
+
     <div class="section bg-navy flex flex-wrap justify-center">
       <div class="section-content" data-aos="fade-up">
         <h2 class="text-white text-3xl">All about Nuxt.js</h2>
@@ -208,6 +214,32 @@
 
 <script>
 import { generateMeta } from '~/utils/meta';
+const specialtyDetails = {
+  text: {
+    h2: 'All about Nuxt.js',
+    h3: 'Nuxt works well for apps big and small thanks to its:',
+    boxedText:
+      'Nuxt.js is an open-source framework used to build Vue.js web applications. It’s a performant system that makes creating universal apps easier because of its reduced routing complexity, giving you SEO benefits right out of the box.',
+    unboxedText: `At Ship Shape, we love Nuxt.js because it’s still an opinionated framework (though not quite as opinionated as Ember.js), and it has features that strip out unused code to make your app fast and lightweight. You’ll enjoy its approachable syntax, easy-to-read read templates, and more. \n \n Whether you're building a brand new web app or elevating an existing experience with the latest best practices, our first-class engineers can refine or create a Nuxt.js app while training your team in the process.`
+  },
+  benefits: [
+    {
+      imgSrc: '/svgs/icons/modular.svg',
+      description: 'Modular architecture, which speeds up development'
+    },
+    {
+      imgSrc: '/svgs/icons/native-apps.svg',
+      description:
+        'Server-side rendered (SSR) to deliver HTML, rather than JavaScript'
+    },
+    {
+      imgSrc: '/svgs/icons/static.svg',
+      description:
+        'Static Site Generation to net SEO benefits without needing a server'
+    }
+  ]
+};
+
 const testimonial = {
   name: 'Donald Piret',
   title: 'Director of Engineering at Sephora',
@@ -222,12 +254,13 @@ const testimonial = {
 
 export default {
   speedkitComponents: {
+    AllAboutSpecialty: () => import('@/components/AllAboutSpecialty'),
     HeroBlock: () => import('@/components/HeroBlock'),
     SingleQuote: () => import('@/components/SingleQuote'),
     WaveMoreLinks: () => import('@/components/WaveMoreLinks')
   },
   data() {
-    return { testimonial };
+    return { testimonial, specialtyDetails };
   },
   head() {
     const title = 'Nuxt.js Software Consultants & Developers';
