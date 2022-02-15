af42bea23a2e709e5ffad4089eb2e16345133fc3
hannakim91 <hannakim91@gmail.com>
2022-02-14 22:00:26 -0800

    update: account for some pages having light blue / main background paragraph flipped

--- components/AllAboutSpecialty.vue
+++ components/AllAboutSpecialty.vue
@@ -2,29 +2,27 @@
 <template>
   <div class="section bg-navy flex flex-wrap justify-center">
     <div class="section-content">
-      <div class="grid grid-cols-1 gap-20 mb-32 lg:gap-32 lg:grid-cols-2">
-        <div class="text-white" data-aos="fade-up">
-          <h2 class="mb-10 text-4xl text-white">{{ text.h2 }}</h2>
+      <h2 class="text-white text-4xl" data-aos="fade-up">
+        {{ text.h2 }}
+      </h2>
+      <div
+        class="
+          grid
+          space-y-12
+          pb-28
+          lg:pt-4 lg:grid-cols-2 lg:gap-32 lg:space-y-0
+        "
+        data-aos="fade-up"
+        data-aos-delay="200"
+      >
+        <div :class="flipParagraphs ? 'order-last pt-12 lg:pt-0' : ''">
           <slot name="unboxed-text">
-            <p class="whitespace-pre-line">
+            <p class="text-white whitespace-pre-line lg:content-center">
               {{ text.unboxedText }}
             </p>
           </slot>
         </div>
-
-        <div
-          class="
-            bg-blue-light
-            flex
-            items-center
-            p-12
-            rounded-md
-            text-navy
-            lg:mt-16
-          "
-          data-aos="fade-up"
-          data-aos-delay="200"
-        >
+        <div class="bg-blue-light flex items-center p-12 rounded-md text-navy">
           <slot name="boxed-text">
             <p class="mb-0">
               {{ text.boxedText }}
@@ -34,9 +32,8 @@
       </div>
 
       <h3 class="text-white text-2xl" data-aos="fade">
-        Some of our favorite Next.js features include:
+        {{ text.h3 }}
       </h3>
-
       <BenefitsList :benefits="benefits" />
     </div>
   </div>
