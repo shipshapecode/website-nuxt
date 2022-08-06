9cd5412e965f92d043d24f9052f14c1c38ec134e
Robert Wagner <rwwagner90@gmail.com>
2022-08-06 19:10:49 -0400

    Start moving images to assets, add employees (#247)

---
+++ content/employee-info.js
@@ -0,0 +1,82 @@
+export const employeeInfo = {
+  mgmt: [
+    {
+      name: 'Robbie Wagner',
+      title: 'CEO',
+      imgSrc: '/img/blog/authors/robbie.jpg',
+      twitter: 'https://twitter.com/rwwagner90',
+      linkedin: 'https://www.linkedin.com/in/robertwilliamwagner/',
+      github: 'https://github.com/rwwagner90',
+      blurb:
+        'Since 2012, Robbie has been using Ember to craft bespoke web applications for companies large and small. He founded Ship Shape in 2017 to provide Ember Consulting for clients and has since ventured into Nuxt and Astro as well. He is a huge Tailwind fanboy and believes it solves CSS once and for all.'
+    },
+    {
+      name: 'Chuck Carpenter',
+      title: 'COO',
+      imgSrc: '/img/blog/authors/chuck.jpg',
+      twitter: 'https://twitter.com/charleswthe3rd',
+      linkedin: 'https://www.linkedin.com/in/chuckcarpenter/',
+      github: 'https://github.com/chuckcarpenter',
+      blurb:
+        'Chuck leads teams to deliver high quality, nimble, reliable, and valuable products in order to create demand and increase their user base. He sometimes makes stuff in Javascript, Serverless, and GraphQL.'
+    }
+  ],
+  team: [
+    {
+      name: 'Rob Cary',
+      title: 'Lead Engineer',
+      imgSrc: '/img/blog/authors/rob.jpg',
+      twitter: 'https://twitter.com/r0bc4ry',
+      linkedin: 'https://www.linkedin.com/in/r0bc4ry/',
+      github: 'https://github.com/r0bc4ry'
+    },
+    {
+      name: 'Jared Galanis',
+      title: 'Senior Software Engineer',
+      imgSrc: '/img/blog/authors/jared.jpg',
+      twitter: 'https://twitter.com/jaredgalanis',
+      linkedin: 'https://www.linkedin.com/in/jaredgalanis/',
+      github: 'https://github.com/jaredgalanis'
+    },
+    {
+      name: 'Nic Romero',
+      title: 'Senior Software Engineer',
+      imgSrc: '/img/blog/authors/nic.jpg',
+      twitter: 'https://twitter.com/ncrmro',
+      linkedin: 'https://www.linkedin.com/in/ncrmro/',
+      github: 'https://github.com/ncrmro'
+    },
+    {
+      name: 'Hanna Kim',
+      title: 'Software Engineer',
+      imgSrc: '/img/blog/authors/hanna.jpg',
+      twitter: '',
+      linkedin: 'https://www.linkedin.com/in/kimhanna/',
+      github: 'https://github.com/hannakim91'
+    },
+    {
+      name: 'Marika Shanahan',
+      title: 'Software Engineer',
+      imgSrc: '/img/blog/authors/marika.jpg',
+      twitter: '',
+      linkedin: 'https://www.linkedin.com/in/marika-shanahan/',
+      github: 'https://github.com/monshan'
+    },
+    {
+      name: 'Krista Williams',
+      title: 'Software Engineer',
+      imgSrc: '/img/blog/authors/krista.jpg',
+      twitter: '',
+      linkedin: 'https://www.linkedin.com/in/kristaawilliams/',
+      github: 'https://github.com/Rystakei'
+    },
+    {
+      name: 'Eli Nicholson',
+      title: 'Software Engineer',
+      imgSrc: '/img/blog/authors/eli.jpg',
+      twitter: 'https://twitter.com/Eli__Nicholson',
+      linkedin: 'https://www.linkedin.com/in/eli-nicholson/',
+      github: 'https://github.com/EmNicholson93'
+    }
+  ]
+};
